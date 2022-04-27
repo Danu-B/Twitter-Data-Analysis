c2f1e9ca99779c2329c1b1a6c9d5e46dd624edc2
Danu-B <dbulom12@gmail.com>
2022-04-27 22:46:20 +0300

    Data exploration and pre-processing

--- tests/test_extract_dataframe.py
+++ tests/test_extract_dataframe.py
@@ -35,13 +35,13 @@ class TestTweetDfExtractor(unittest.TestCase):
                 "Thank you @research2note for creating this amazing campaign &amp; turning social media #red4research today. @NHSRDFORUM is all about sharing the talent, passion  &amp; commitment of individuals coming together as a community for the benefit of all. You've done this. Well done 👋", 'Former Pfizer VP and Virologist, Dr. Michael Yeadon, is one of the most credentialed medical professionals speaking out about the dangers of the #Covid19 vaccines, breaks down his “list of lies” that keeps him up at night. https://t.co/LSE8CrKdqn', 'I think it’s important that we don’t sell COVAX short. It still has a lot going for it and is innovative in its design. But it needs more vaccines to share.  We’re hoping our low cost @TexasChildrens recombinant protein COVID19 vaccine with @biological_e will help fill some gaps']
 
         self.assertEqual(self.df.find_full_text(), text)
-"""
+
     def test_find_sentiments(self):
         self.assertEqual(self.df.find_sentiments(self.df.find_full_text()), (
             [0.16666666666666666, 0.13333333333333333, 0.3166666666666667, 0.08611111111111111, 0.27999999999999997],
             [0.18888888888888888, 0.45555555555555555, 0.48333333333333334, 0.19722222222222224, 0.6199999999999999], 
             ['positive', 'positive', 'positive', 'positive', 'positive']))
-
+"""
     def test_find_created_time(self):
         created_at = ['Fri Jun 18 17:55:49 +0000 2021', 'Fri Jun 18 17:55:59 +0000 2021', 'Fri Jun 18 17:56:07 +0000 2021',
                       'Fri Jun 18 17:56:10 +0000 2021', 'Fri Jun 18 17:56:20 +0000 2021']
